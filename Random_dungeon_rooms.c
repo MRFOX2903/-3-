bbab1ab73017bdfd1a8139ef9cebc5bb97e16c34
MRFOX2903 <g.yunoshev@g.nsu.ru>
2026-01-21 19:49:52 +0300

    Final_Random_dungeon_rooms.c

--- Random_dungeon_rooms.c
+++ Random_dungeon_rooms.c
@@ -66,7 +66,7 @@ void roomWithGreatArmor() {
     printf("=================================================================\n");
     printf(" An unexplored room:\n");
     printf(" Walking through the tunnels, you suddenly enter a shining room...\n");
-    printf(" In the middle of the room, there is a high-quality armor on a pedestal.");
+    printf(" In the middle of the room, there is a high-quality armor on a pedestal.\n");
     printf("=================================================================\n\n");
     findArmor(30, 40, 6, 10, 100, 100);
 }
@@ -82,4 +82,123 @@ void moneyRoom() {
     printf(" by cleaning them from dust, you put the find in your pocket.....\n");
     printf("=================================================================\n\n");
     printf(" in this room, your balance has increased on '%d' gold", money);
+}
+
+void roomWithWeakMonster() {
+    showPlayerStats(&globalPlayer);
+    printf("------------------------CURRENT ROOM-----------------------------\n");
+    printf("=================================================================\n");
+    printf(" A dangerous room with monster:\n");
+    printf(" As you round the corner, you spot a goblin...\n");
+    printf(" Unfortunately, the goblins are very agile, and you couldn't avoid the battle..\n");
+    printf("=================================================================\n\n");
+
+    printf("Choose a fighting style:\n");
+    printf(" 1 - Precise (small damage but lesser reward)\n");
+    printf(" 2 - Aggressive (combat and rewards become unpredictable)\n");
+    printf("\nEnter your choice: ");
+
+    int Choice;
+    int takenDamage;
+    int reward;
+    int battleResult;
+
+    scanf("%d", &Choice);
+    while (getchar() != '\n');
+
+    if (Choice == 1) {
+        takenDamage = getRandomInt(3, 15);
+
+        int actualDamage = takenDamage - globalPlayer.total_defense;
+        if (actualDamage < 0) {
+            actualDamage = 0;
+        }
+
+        reward = getRandomInt(30, 120);
+
+        if (actualDamage == 0) {
+            printf("\n Your armor protected you! No damage taken.\n");
+        }
+        else {
+            printf("\n You lost %d HP!\n", actualDamage);
+            globalPlayer.lost_hp += actualDamage;
+        }
+
+        armorDamage(&globalPlayer, 2);
+        globalPlayer.cash += reward;
+        printf(" Received reward: %d gold\n", reward);
+
+    }
+
+    else {
+        battleResult = getRandomInt(1, 10);
+
+        if (battleResult >= 1 && battleResult <= 5) {
+
+            reward = getRandomInt(60, 160);
+            printf("\n Perfect victory! You defeated the goblin quickly.\n");
+            printf(" No damage taken, but armor worn out.\n");
+            printf(" Received reward: %d gold\n", reward);
+
+            armorDamage(&globalPlayer, 1);
+            globalPlayer.cash += reward;
+
+        }
+        else if (battleResult >= 6 && battleResult <= 9) {
+
+            takenDamage = getRandomInt(8, 18);
+            reward = getRandomInt(30, 130);
+
+            int actualDamage = takenDamage - globalPlayer.total_defense;
+            if (actualDamage < 0) {
+                actualDamage = 0;
+            }
+
+            if (actualDamage == 0) {
+                printf("\n Your armor blocked the attack!\n");
+            }
+            else {
+                printf("\n You lost %d HP in the fight!\n", actualDamage);
+                globalPlayer.lost_hp += actualDamage;
+            }
+
+            armorDamage(&globalPlayer, 4);
+            globalPlayer.cash += reward;
+            printf(" Received reward: %d gold\n", reward);
+
+        }
+        else if (battleResult == 10) {
+
+            takenDamage = getRandomInt(20, 30);
+            reward = getRandomInt(30, 120);
+
+            int actualDamage = takenDamage - globalPlayer.total_defense;
+            if (actualDamage < 1) {
+                actualDamage = 1;
+            }
+
+            printf("\n Critical hit! The goblin struck you hard!\n");
+            printf(" You lost %d HP!\n", actualDamage);
+
+            globalPlayer.lost_hp += actualDamage;
+            armorDamage(&globalPlayer, 6);
+            globalPlayer.cash += reward;
+
+            printf(" Received minimal reward: %d gold\n", reward);
+        }
+    }
+
+    recalculateStats(&globalPlayer);
+}
+
+void cursedArmorRoom() {
+    int cursedDamage = getRandomInt(5, 10);
+    showPlayerStats(&globalPlayer);
+    printf("------------------------CURRENT ROOM-----------------------------\n");
+    printf("=================================================================\n");
+    printf(" A room with a strange fog\n");
+    printf(" You are under the influence of a strange curse\n");
+    printf(" Your armor has lost %d durability.\n", cursedDamage);
+    armorDamage(&globalPlayer, cursedDamage);
+    printf("=================================================================\n\n");
 }
